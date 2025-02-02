d9e97b67a924ebffbf5175c1de10ed6118bcda3b
PawanSenapati <pawansenapati1999@gmail.com>
2025-02-02 19:01:38 +0000

    [ADD] Modify Github Workflow

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -1,6 +1,6 @@
 module "cloud_run" {
-  source     = "GoogleCloudPlatform/cloud-run/google"
-  version    = "~> 0.14"
+  source  = "GoogleCloudPlatform/cloud-run/google"
+  version = "~> 0.14"
 
   service_name = var.cr_service_name
   project_id   = local.project_id
@@ -11,6 +11,10 @@ module "cloud_run" {
     cpu    = var.cr_cpu
     memory = var.cr_memory
   }
+  ports = ({
+    "name" : "http1",
+    "port" : 5000
+  })
   service_annotations = {
     "run.googleapis.com/ingress" : var.cr_ingress_setting
   }
