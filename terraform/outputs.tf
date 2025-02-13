c84434581f29fa482b75806708f9614f3c86b9b7
PawanSenapati <pawansenapati1999@gmail.com>
2025-02-13 14:47:03 +0000

    [ADD] Output the frontend ip of the load balancer

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "cloud_run_url" {
-    value = module.cloud_run.service_url
+  value = module.cloud_run.service_url
+}
+
+output "load_balancer_frontend_ip" {
+  value = module.load-balancer.load_balancer_frontend_ip
 }
